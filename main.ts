876bbf66580a9fc9020f9bfa9de053a929fa63c0
PC-HK <78074625+PC-HK@users.noreply.github.com>
2021-03-24 17:02:43 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,15 @@
 let nop = 0
-let stable = 0
 basic.forever(function () {
     if (pins.analogReadPin(AnalogPin.P0) < 200) {
         nop += 1
-        if (stable == 0) {
-            stable += 1
-        }
     }
     if (pins.analogReadPin(AnalogPin.P1) < 200) {
         nop += 1
-        if (stable != 2) {
-            stable += 1
-        }
     }
-    basic.showNumber(stable)
-    if (stable == 1 || stable == 2) {
-        stable = 0
-    }
+    basic.showNumber(nop)
+    basic.pause(200)
     nop = 0
+})
+basic.forever(function () {
+    basic.pause(1000)
 })
